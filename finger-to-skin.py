697bb2b59dd30c53a337338793be332d670751eb
AkiraMorilas <134231050+AkiraMorilas@users.noreply.github.com>
2023-05-26 07:40:26 +0200

    v0.2
    TODO: figure out git so I don't need the browser to upload

--- finger-to-skin.py
+++ finger-to-skin.py
@@ -5,11 +5,15 @@
 # else it'll throw "invalid decimal literal" errors
 # that's why it's "k1" instead of "1k" and so on
 
-thumb = input("please input your thumb note: ")
-index = input("please input your index note: ")
-middle = input("please input your middle note: ")
-ring = input("please input your ring note: ")
-pinky = input("please input your pinky note: ")
+thumb = input("input your thumb note: ")
+index = input("input your index note: ")
+middle = input("input your middle note: ")
+ring = input("input your ring note: ")
+pinky = input("input your pinky note: ")
+width = input("how wide are the notes supposed to be: ")
+hitpos = input("what is your hit position: ")
+scorepos = input("what is your score position: ")
+combopos = input("what is your combo position: ")
 
 k1 = [thumb]
 k2 = [index,index]
@@ -27,8 +31,29 @@ k18 = [pinky,ring,middle,index,pinky,ring,middle,index,thumb,thumb,index,middle,
 
 keycounts = [k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k13,k17,k18]
 
+def zeroes(n):
+    r = ""
+    for i in n:
+        r += "0, "
+    return r
+
 for i in keycounts:
-    print(str(len(i)) + "K")
+    print("[Mania]")
+    print("Keys: " + str(len(i)))
+    print("")
+    print("TODO: other things for generating the whole skin.ini")
+    print("")
+    #CollumnStart: <- need to know the centering algorithm
+    #ColumnWidth: <- need to know the centering algorithm
+    print("ColumnLineWidth: " + zeroes(i))
+    print("SplitStages: 0")
+    print("HitPosition: " + str(hitpos))
+    #LightPosition: <- do I need this even?
+    #BarlineHeight: <- TODO figure this out
+    #JudgementLine: <- TODO figure this out
+    #NoteBodyStyle: <- TODO figure this out
+    print("ScorePosition: " + str(scorepos))
+    print("ComboPosition: " + str(combopos))
     count = 0
     for j in i:
         print("NoteImage" + str(count) + ": " + str(j))
@@ -36,5 +61,6 @@ for i in keycounts:
         print("NoteImage" + str(count) + "L: " + str(j))
         print("NoteImage" + str(count) + "T: " + str(j))
         count+=1
+    print("")
 
 
